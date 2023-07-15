5d1d1c33b5ae88056208f72ed3886a4307f22418
gyan7 <gyansinghbudhiraja777@gmail.com>
2023-07-15 15:44:19 +0530

    add4

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,6 @@
  echo "BUILD START"
  python3.9 -m pip install -r requirements.txt
  python3.9 manage.py collectstatic --noinput --clear
- python3.9 manage.py makemigrations
- python3.9 manage.py migrate
- vim var/task/django/db/backends/sqlite3/base.py
- # from sqlite3 import dbapi2 as Database # annotation
- from pysqlite3 import dbapi2 as Database # import pysqlite3
+ python3.9 manage.py makemigrations --noinput
+ python3.9 manage.py migrate --noinput
  echo "BUILD END"
